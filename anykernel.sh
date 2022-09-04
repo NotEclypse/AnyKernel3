e4d3e73b1e8f5215cf2a61e3dff9cb38113b2a9a
NotEclypse <szabence@protonmail.com>
2022-09-04 10:38:39 +0200

    Liquid-ify!

--- anykernel.sh
+++ anykernel.sh
@@ -4,19 +4,20 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Predator Stormbreaker Kernel  by sohamsen  @ xda-developers
+kernel.string=Liquid Kernel by NotEclypse
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=lavender
-supported.versions=9 - 12
+device.name1=sweet
+device.name2=sweetin
+supported.versions=11 - 13
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+block=/dev/block/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
